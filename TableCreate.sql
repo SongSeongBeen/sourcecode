ca0da9b5bc298ae4e19933b2a864f9cbb40155c3
송성빈 <aktm1004@naver.com>
2022-01-06 18:19:04 +0900

    Create

--- TableCreate.sql
+++ TableCreate.sql
@@ -58,6 +58,48 @@ order by book_id asc;
 
 
 
+--테이블 삭제
+drop table guestbook;
+
+--시퀀스 삭제
+drop sequence seq_guestbook;
+
+
+create table guestbook(
+    no	     number(10),
+    name  	 varchar2(20),
+    password varchar2(20),
+    content	 varchar2(2000),
+    reg_date date,
+    PRIMARY KEY(no)	
+);
+
+--시퀀스 생성
+CREATE SEQUENCE seq_guestbook
+INCREMENT BY 1 
+start with 1 
+nocache;
+
+insert into guestbook
+values(seq_guestbook.nextval, '송성빈', '1234', '출췍완료', sysdate);
+insert into guestbook
+values(seq_guestbook.nextval, '전지현', '1234', '출췍완료', sysdate);
+insert into guestbook
+values(seq_guestbook.nextval, '장동건', '1234', '출췍완료', sysdate);
+insert into guestbook
+values(seq_guestbook.nextval, '원빈', '1234', '출췍완료', sysdate);
+insert into guestbook
+values(seq_guestbook.nextval, '임수정', '1234', '출췍완료', sysdate);
+
+commit;
+
+select no,
+       name,
+       password,
+       content,
+       to_char(reg_date, 'YYYY-MM-DD hh:mi:ss') reg_date
+from guestbook
+order by reg_date desc;
 
 
 
