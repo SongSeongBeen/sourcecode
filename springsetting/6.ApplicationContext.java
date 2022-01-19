12d8f74510256fdaa3d4c66424b12b3e60de8159
송성빈 <aktm1004@naver.com>
2022-01-19 20:32:06 +0900

    spring

--- springsetting/6.ApplicationContext.java
+++ springsetting/6.ApplicationContext.java
@@ -1,8 +1,8 @@
+
+//ApplicationContext.xml 파일생성후 붙여넣기
 <?xml version="1.0" encoding="UTF-8"?>
-<beans xmlns="http://www.springframework.org/schema/beans"
-xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:aop="http://www.springframework.org/schema/aop"
-xmlns:context="http://www.springframework.org/schema/context"
-xsi:schemaLocation="http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans.xsd
+<beans xmlns="http://www.springframework.org/schema/beans" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:aop="http://www.springframework.org/schema/aop" xmlns:context="http://www.springframework.org/schema/context"
+   xsi:schemaLocation="http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans.xsd
         http://www.springframework.org/schema/aop http://www.springframework.org/schema/aop/spring-aop.xsd
         http://www.springframework.org/schema/context http://www.springframework.org/schema/context/spring-context.xsd
         http://www.springframework.org/schema/jee http://www.springframework.org/schema/jee/spring-jee.xsd
@@ -15,13 +15,26 @@ xsi:schemaLocation="http://www.springframework.org/schema/beans http://www.sprin
         http://www.springframework.org/schema/task
         http://www.springframework.org/schema/task/spring-task-3.0.xsd">
 
-    <context:annotation-config />
+   <context:annotation-config />
 
-    <context:component-scan base-package="com.javaex">
-            <context:include-filter type="annotation" expression="org.springframework.stereotype.Repository" />
-            <context:include-filter type="annotation" expression="org.springframework.stereotype.Service" />
-            <context:include-filter type="annotation" expression="org.springframework.stereotype.Component" />
-    </context:component-scan>
+   <context:component-scan base-package="com.javaex">
+      <context:include-filter type="annotation" expression="org.springframework.stereotype.Repository" />
+      <context:include-filter type="annotation" expression="org.springframework.stereotype.Service" />
+      <context:include-filter type="annotation" expression="org.springframework.stereotype.Component" />
+   </context:component-scan>
 
 </beans>
+
+
+///////////////////////////ApplicationContext 경로값 xml에 등록//////////////////////////////
+//Xml 파일에 넣기
+<!-- Context Listener 등록 -->
+<listener>
+<listener-class>org.springframework.web.context.ContextLoaderListener</listener-class>
+</listener>
+
+<context-param>
+<param-name>contextConfigLocation</param-name>
+<param-value>/WEB-INF/applicationContext.xml</param-value>
+</context-param>
 
