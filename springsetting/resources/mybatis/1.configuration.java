b5b1bcfb4f6bca27471b4cd81112a65d0a1542dd
송성빈 <aktm1004@naver.com>
2022-04-06 23:37:17 +0900

    sourcecode

--- springsetting/resources/mybatis/1.configuration.java
+++ springsetting/resources/mybatis/1.configuration.java
@@ -1,4 +1,4 @@
-//mybitas폴더에 xml로 파일 만들어서 붙여 넣기
+//mybitas폴더에 configuration xml로 파일 만들어서 붙여 넣기
 
 <?xml version="1.0" encoding="UTF-8" ?>
 <!DOCTYPE configuration PUBLIC "-//mybatis.org//DTD Config 3.0//EN" "http://mybatis.org/dtd/mybatis-3-config.dtd">
